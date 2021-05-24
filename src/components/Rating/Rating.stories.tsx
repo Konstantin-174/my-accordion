2e4e80a8cae63240fc746afe767c5b74bb58ac02
Konstantin-174 <timokhin_74@hotmail.com>
2021-05-24 21:25:41 +0500

    13th (Trying to use Storybook)

---
+++ src/components/Rating/Rating.stories.tsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import {Rating} from './Rating';
+import {action} from '@storybook/addon-actions';
+import {RatingValuesType} from '../../App';
+
+export default {
+    title: 'TestRating',
+    component: Rating
+}
+
+export const EmptyRating = () => <Rating value={0} onClick={action("Rating clicked!")}/>
+export const Rating1 = () => <Rating value={1} onClick={action("Rating clicked!")}/>
+export const Rating2 = () => <Rating value={2} onClick={action("Rating clicked!")}/>
+export const Rating3 = () => <Rating value={3} onClick={action("Rating clicked!")}/>
+export const Rating4 = () => <Rating value={4} onClick={action("Rating clicked!")}/>
+export const Rating5 = () => <Rating value={5} onClick={action("Rating clicked!")}/>
+export const TestRating = () => {
+    const [value, setValue] = useState<RatingValuesType>(0)
+    return <Rating value={value} onClick={setValue}/>
+}
